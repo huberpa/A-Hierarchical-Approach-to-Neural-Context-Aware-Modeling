246c593882907881e15f0d1f4ceecfdfcd32bd20
huberpa <patrick.huber.gaggenau@freenet.de>
2017-10-19 10:30:49 +0200

    new generative model

--- 6_experimental/generative_Inference.py
+++ 6_experimental/generative_Inference.py
@@ -8,6 +8,9 @@ parser.add_option('--network_path', action="store", dest="network_path", help="T
 parser.add_option('--data_path', action="store", dest="data_path", help="The path to the word index relation (default: '.')", default=".")
 parser.add_option('--sentence_enc_path', action="store", dest="sentence_enc_path", help="The path to the baseline model for the sentence embedding - NEEDS TO BE A KERAS MODEL (Gen 8, 1e-3) (default: '.')", default=".")
 parser.add_option('--sentence_log', action="store", dest="sentence_log", help="The path to the sentence embeddings logfiles (default: .)", default=".")
+parser.add_option('--max_sentence_embeddings', action="store", dest="max_sentence_embeddings", help="The number of previous sentences that are taken into account to compute the next sentence (default: 5)", default="5")
+parser.add_option('--nr_sentences', action="store", dest="nr_sentences", help="The number of new sentence to create (default: 10)", default="10")
+parser.add_option('--save', action="store", dest="save", help="The file to save the result to (default: ./createdStory.txt)", default="./createdStory.txt")
 
 options, args = parser.parse_args()
 model_path = options.network_path
@@ -15,6 +18,9 @@ data_path = options.data_path
 sentence_log = options.sentence_log
 story_file = options.story_file
 sentence_enc_path = options.sentence_enc_path
+save = options.save
+max_sentence_embeddings = int(options.max_sentence_embeddings)
+number_sentences = int(options.nr_sentences)
 ##############################################
 
 
@@ -39,7 +45,7 @@ sys.setdefaultencoding('utf-8')
 ##############################################
 
 # Load files
-print "Reading network input data..."
+#print "Reading network input data..."
 with open (data_path+"/index_to_word.txt", 'r') as f:
 	index_to_word = json.load(f)
 with open (data_path+"/word_to_index.txt", 'r') as f:
@@ -72,7 +78,7 @@ sentenceModel = load_model(sentence_enc_path)
 newSentenceModel = Sequential()
 newSentenceModel.add(Embedding(input_dim=len(word_to_index), output_dim=embedding_size, mask_zero=True, weights=sentenceModel.layers[0].get_weights()))
 for layerNumber in range(0, nb_hidden_layers):
-	print "add LSTM layer...."
+	#print "add LSTM layer...."
 	newSentenceModel.add(LSTM(units=hidden_dimensions, return_sequences=True, weights=sentenceModel.layers[layerNumber+1].get_weights()))
 newSentenceModel.compile(loss='sparse_categorical_crossentropy', optimizer="adam")
 
@@ -82,10 +88,9 @@ for sentence in word_story:
 		trainInput[0, index] = word
 	sentence_embeddings.append(newSentenceModel.predict(trainInput, verbose=0)[0][-1].tolist())
 
-max_sentence_embeddings = 10
-number_sentences = 10
-
-
+with open("./createdStory.txt", "w") as f:
+	f.write(story)
+	print story
 for _ in range (0, number_sentences):
 
 	encoder_length = len(sentence_embeddings)
@@ -95,45 +100,40 @@ for _ in range (0, number_sentences):
 	for _ in range(0, 10-len(sentence_embeddings)):
 		sentence_input.append(empty)
 
-	print "Launch the graph..."
+	#print "Launch the graph..."
 	session_config = tf.ConfigProto(allow_soft_placement=True)    
 	session_config.gpu_options.per_process_gpu_memory_fraction = 0.90
 	with tf.Session(config=session_config) as session:
 		tf.train.import_meta_graph(model_path + ".meta").restore(session, model_path)
 		variables = tf.get_collection('variables_to_store')
 
-		print "Start Creating..."
+		#print "Start Creating..."
 		feed = {}
 		feed["encoder_inputs"] = [sentence_input]
 		feed["encoder_length"] = [encoder_length]
 		feed["decoder_length"] = [decoder_length]
 		feed["start_token_infer"] = [word_to_index[start_token]]
 
-		#print feed["encoder_inputs"]
-		#print feed["encoder_length"]
-		#print feed["decoder_length"]
-		#print feed["start_token_infer"]
-
-		sentence_output = session.run(variables[9], feed_dict={variables[3]:feed["encoder_inputs"], variables[7]: feed["encoder_length"], variables[8]: feed["decoder_length"], variables[10]: feed["start_token_infer"]})[0]
+		output = session.run(variables[9], feed_dict={variables[3]:feed["encoder_inputs"], variables[7]: feed["encoder_length"], variables[8]: feed["decoder_length"], variables[10]: feed["start_token_infer"]})[0]
 		sentence_output = []
-		for word in sentence_output:
+		for word in output:
 			if word != 0:
-				sentence_output = sentence_output + [index_to_word[str(word)]]
+				sentence_output += [index_to_word[str(word)]]
 
-		print sentence_output
 		#save in file
-		with open(story_file, "a") as f:
+		with open(save, "a") as f:
 			string_sentence = ""
-			for element in sentence_output:
-				string_sentence += string_sentence + " " + element	
-			print "write into file: "+string_sentence
-			f.write("{}\n".format(string_sentence))
+			for element in sentence_output[:-1]:
+				string_sentence += " " + element	
+			print string_sentence
+			f.write(string_sentence)
 
-	trainInput = np.zeros((1, len(sentence_output)), dtype=np.int16)
+	trainInput = np.zeros((1, len(output)+1), dtype=np.int16)
+	trainInput[0, 0] = word_to_index["<START>"]
 	for index, word in enumerate(output):
-		trainInput[0, index] = word
+		trainInput[0, index+1] = word
 	sentence_embeddings.append(newSentenceModel.predict(trainInput, verbose=0)[0][-1].tolist())
-	if len(sentence_embeddings) > 10:
+	if len(sentence_embeddings) > max_sentence_embeddings:
 		sentence_embeddings = sentence_embeddings[1:]
 
 ##############################################
